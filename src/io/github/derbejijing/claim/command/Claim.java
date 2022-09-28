7cd9d863ef18c063bb27a8a4bbf96f09ceec5a23
DerBejijing <verunsicherungsgmbhundco@gmail.com>
2022-09-28 16:52:07 +0200

    add claim chunk command

--- src/io/github/derbejijing/claim/command/Claim.java
+++ src/io/github/derbejijing/claim/command/Claim.java
@@ -3,17 +3,31 @@ package io.github.derbejijing.claim.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import io.github.derbejijing.claim.chunk.ChunkManager;
+import io.github.derbejijing.claim.storage.DataStorage;
+import net.md_5.bungee.api.ChatColor;
 
 public class Claim implements CommandExecutor {
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(args.length == 1) {
-            if(args[0] == "start") {
+        if(sender instanceof Player) {
+            if(args.length == 0) {
+                if(DataStorage.team_get_by_player(sender.getName()) == null) {
+                    sender.sendMessage(ChatColor.RED + "You are not in a team");
+                    return true;
+                }
+                ChunkManager.claim_chunk((Player)sender, ((Player)sender).getLocation().getChunk());
+            }
+            else if(args.length == 1) {
+                if(args[0] == "start") {
 
-            } else if(args[0] == "stop") {
+                } else if(args[0] == "stop") {
 
-            } else return false;
+                } else return false;
+            }
         }
         return true;
     }
